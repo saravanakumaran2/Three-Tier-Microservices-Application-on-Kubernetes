e20172fea5e340d89831c27e06e9bb18b44f5a04
Your Name <you@example.com>
2025-07-22 22:02:22 +0000

    Updating all the files

--- backend/server.py
+++ backend/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="mysql",
+        host=os.environ.get("MYSQL_HOST", "mysql"),
         user=os.environ['MYSQL_USER'],
         password=os.environ['MYSQL_PASSWORD'],
         database=os.environ['MYSQL_DATABASE']
